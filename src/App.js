f71a1e3b2dea763f753ab054762c70dcc3d5f163
Natali27M <vns122716@gmail.com>
2022-01-14 18:28:22 +0200

    класна 2

--- src/App.js
+++ src/App.js
@@ -1,31 +1,33 @@
-import {useState} from "react";
+// import {useState} from "react";
 
 import './App.css';
 import Users from './components/Users/Users'
 
 const App = () => {
-    const [form, setForm] = useState({name:'',surname:''});
-
-    const find = (e) => {
-        e.preventDefault();
-        console.log(<Users/>);
-    };
-
-    const formHandler = (e) => {
-        setForm({...form, [e.target.name]: e.target.value});
-    };
+    // const [form, setForm] = useState({name:'',surname:''});
+    //
+    // const find = (e) => {
+    //     e.preventDefault();
+    //     console.log(e.target.name.value);
+    // };
+    //
+    // const formHandler = (e) => {
+    //     setForm({...form, [e.target.name]: e.target.value});
+    // };
 
     return (
         <div>
-            <form onSubmit={find}>
-                <div><label>Name : <input type="text" name={'name'} value={form.name} onChange={formHandler}/></label></div>
-                <div><label>Surname : <input type="text" name={'surname'} value={form.surname} onChange={formHandler}/></label></div>
-                <button>Find</button>
-            </form>
+            {/*<form onSubmit={find}>*/}
+            {/*    <div><label>Name : <input type="text" name={'name'} value={form.name} onChange={formHandler}/></label></div>*/}
+            {/*    <div><label>Surname : <input type="text" name={'surname'} value={form.surname} onChange={formHandler}/></label></div>*/}
+            {/*    <button>Find</button>*/}
+            {/*</form>*/}
             <Users/>
         </div>
     );
 };
 
 export default App;
+
+
 
--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -4,15 +4,34 @@ import User from '../User/User';
 
 const Users = () => {
     const [users, setUsers] = useState([]);
-    useEffect(()=>{
+    useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(value => value.json())
             .then(value => setUsers(value));
-    },[])
+    }, []);
+
+    const [form, setForm] = useState({name:'',surname:''});
+
+    const find = (e) => {
+        e.preventDefault();
+        console.log(e.target.name.value);
+    };
+
+    const formHandler = (e) => {
+        setForm({...form, [e.target.name]: e.target.value});
+    };
 
     return (
         <div>
-            {users.map(value => <User key={value.id} name={value.name}/>)}
+            <div>
+                <form onSubmit={find}>
+                    <div><label>Name : <input type="text" name={'name'} value={form.name} onChange={formHandler}/></label></div>
+                    <div><label>Surname : <input type="text" name={'surname'} value={form.surname} onChange={formHandler}/></label></div>
+                    <button>Find</button>
+                </form>
+            </div>
+                {users.map(value => <User key={value.id} name={value.name}/>)}
+
         </div>
     );
 };
