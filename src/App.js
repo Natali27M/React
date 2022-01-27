f2731330fded37e2818c40c0861e665da7fdecfd
Natali27M <vns122716@gmail.com>
2022-01-27 21:58:48 +0200

    5

--- src/App.js
+++ src/App.js
@@ -1,12 +1,53 @@
-import Forms from "./components/Forms";
+import {useReducer, useRef} from "react";
+import {type} from "@testing-library/user-event/dist/type";
+import NewCatsDogs from "./components/NewCatsDogs/NewCatsDogs";
 
-const App = () => {
-  return (
-      <div>
-          <Forms/>
-      </div>
-  );
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'add':
+            if (action.target === 'cats') {
+                return {...state, cats: [...state.cats, action.payload]};
+            }
+            return state
+        case 'del':
+            if (action.target === 'cats') {
+                return {...state, cats: state.cats.filter(cat => cat.id !== action.payload)};
+            }
+            return state;
+        default :
+            throw new Error('Error');
+    }
 };
+const App = () => {
+    const [state, dispatch] = useReducer(reducer, {cats: [], dogs: []});
+    const inputCatValue = useRef()
+
+    const addCats = (e) => {
+        e.preventDefault();
+        const newCat = inputCatValue.current.value;
+
+        dispatch({type: 'add', target: 'cats', payload: {name: newCat, id: new Date().getTime()}});
+        console.log(state);
+    }
+
+    const del = (id) => {
+        dispatch({type: 'del', target: 'cats', payload: id})
+    }
+
+    return (
+        <div>
+            <form>
+                <label>Add cat : <input type="text" name={'cats'} ref={inputCatValue}/></label>
+                <button onClick={addCats}>Save</button>
+                <label>Add cat : <input type="text"/></label>
+                <button>Save</button>
+            </form>
+            {state.cats.map(cat => <div key={cat.id}>{cat.name}
+                <button onClick={()=>dispatch({type: 'del', target: 'cats', payload: cat.id})}>delete</button>
+            </div>)}
+        </div>
+    )
+}
 
 export default App;
 
---
+++ src/components/NewCatsDogs/NewCatsDogs.js
@@ -0,0 +1,9 @@
+const NewCatsDogs = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default NewCatsDogs;
